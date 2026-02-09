d841fcb9a16d5575ba6e32f29443bb28398ae61e
brtester2025-collab <brtester2025@gmail.com>
2026-02-09 12:16:37 +0530

    ETA 2

--- tests/middlewear.test.js
+++ tests/middlewear.test.js
@@ -1,1 +1,67 @@
-const makeAuthMiddleware = require('./middlewear.js')
+const { makeAuthMiddleware } = require('./middlewear.js')
+const jwt = require('jsonwebtoken')
+
+jest.mock('jsonwebtoken')
+
+describe('auth middle wear (jest only)', () => {
+    const jwtSecret = 'test-secret'
+    let auth;
+
+    const makeRes = () => {
+        const res = {}
+        res.status = jest.fn().mockReturnValue(res)
+        res.json = jest.fn().mockReturnValue(res)
+        return res
+    }
+
+
+    beforeEach(() => {
+        auth = makeAuthMiddleware({ jwtSecret })
+        jest.clearAllMocks();
+    })
+
+    test('allow request with valid tokens', () => {
+        jwt.verify.mockReturnValue({ userId: 'u1' });
+
+        const req = {
+            headers: { authorization: "Bearer Valid-token" }
+        }
+
+        const res = makeRes();
+        const next = jest.fn();
+
+
+        auth(req, res, next);
+
+        expect(jwt.verify).toHaveBeenCalledWith('Valid-token', jwtSecret)
+        expect(req.user).toEqual({ id: 'u1' });
+        expect(next).toHaveBeenCalledWith()
+        expect(res.status).not.toHaveBeenCalledWith();
+    })
+
+
+    test('token is missing', () => {
+        const req = { header: {} };
+        const res = makeRes();
+        const next = jest.fn();
+
+        auth(req, res, next)
+
+        expect(res.status).toHaveBeenCalledWith(401);
+        expect(res.json).toHaveBeenCalledWith({ message: 'Missing token' });
+        expect(next).not.toHaveBeenCalledWith();
+    })
+
+    test('if token is missing', () => {
+        const req = { header: { authorization: {} } }
+        const res = makeRes();
+        const next = jest.fn();
+
+        auth(req, res, next)
+
+        expect(res.status).toHaveBeenCalledWith(401)
+        expect(res.json).toHaveBeenCalledWith({ message: 'Missing token' })
+        expect(next).not.toHaveBeenCalledWith()
+    })
+});
+
